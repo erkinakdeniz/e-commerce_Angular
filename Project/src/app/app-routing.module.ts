f4633e64633246793e062c0de5892bdf9027fec2
erkinakdeniz <53817303+erkinakdeniz@users.noreply.github.com>
2022-02-10 19:33:42 +0300

    showroom da hata var

--- Project/src/app/app-routing.module.ts
+++ Project/src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ProductDetailsComponent } from './components/product-details/product-details.component';
 import { ProductListComponent } from './components/product-list/product-list.component';
 
 const routes: Routes = [
   {path:'',pathMatch:'full',component:HomeComponent},
   {path:"products",component:ProductListComponent},
+  {path:'detail',component:ProductDetailsComponent},
   {path:"**",component:HomeComponent}];
 
 @NgModule({
